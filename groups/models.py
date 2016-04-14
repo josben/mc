8a93bb3f185fe44e562f7ccce274906b77b1be02
Benjamin Perez <josebenjaminp@gmail.com>
2016-04-13 20:15:54 -0400

    add groups

---
+++ groups/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from contacts.models import Contact
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+
+class Group_Contact(models.Model):
+    group = models.ForeignKey(Group)
+    contact = models.ForeignKey(Contact)
+
